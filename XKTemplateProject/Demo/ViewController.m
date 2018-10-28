8fbef35632c11d29a4df14ce32e94d84c6de5d99
浪漫恋星空 <727378500@qq.com>
2018-10-28 17:40:32 +0800

    2

---
+++ XKTemplateProject/Demo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  XKTemplateProject
+//
+//  Created by 浪漫恋星空 on 2018/10/28.
+//  Copyright © 2018 浪漫恋星空. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
